86cefa95ae09988f69a9a3e501ee67f3119a965f
diogo <diogo.pimentel@exset.com>
2021-04-23 10:12:17 +0200

    Adding support for remote devices (network) on fpicker.

--- fpicker.h
+++ fpicker.h
@@ -1,9 +1,4 @@
-#ifdef __linux__
-    #include "frida-core-linux.h"
-#else
-    #include "frida-core.h"
-#endif
-
+#include "frida-core.h"
 #include <syslog.h>
 #include <stdint.h>
 #include <stdbool.h>
@@ -77,6 +72,7 @@ typedef enum EXEC_MODE {
 
 typedef enum DEVICE {
     DEVICE_LOCAL,
+    DEVICE_REMOTE,
     DEVICE_USB
 } device_t;
 
@@ -149,7 +145,7 @@ typedef struct _coverage_t {
 typedef struct _bb_entry_t {
    uint32_t start;
    uint16_t size;
-   uint16_t mod_id; 
+   uint16_t mod_id;
 } bb_entry_t;
 
 typedef struct _module_t {
@@ -223,7 +219,7 @@ struct timeval *_start_measure();
 uint64_t _stop_measure(struct timeval *t);
 
 // AFL Fuzzer Mode Functions
-#define FORKSRV_FD 198 
+#define FORKSRV_FD 198
 void run_forkserver(fuzzer_state_t *fstate);
 
 // Standalone Mode Stuff
--- harness/stalker-instrumentation.js
+++ harness/stalker-instrumentation.js
@@ -4,6 +4,8 @@ if (Process.arch == "x64") {
   pc = "rip";
 } else if (Process.arch == "arm64") {
   pc = "pc";
+} else if (Process.arch == "arm") {
+  pc = "pc";
 } else {
   console.log("[!] Unknown architecture!");
 }
@@ -23,7 +25,7 @@ module.exports = new CModule(`
     uintptr_t prev_loc;
     // pointer to a JS NativeCallback that calls console.log, do
     // not use in production
-    void (*log)(long); 
+    void (*log)(long);
   };
 
   bool is_within_module(uintptr_t pc, uintptr_t s, uintptr_t e) {
@@ -33,7 +35,7 @@ module.exports = new CModule(`
   void transform (GumStalkerIterator * iterator, GumStalkerOutput * output, gpointer user_data) {
     cs_insn * insn;
     struct _user_data *ud = (struct _user_data*)user_data;
-    
+
     gum_stalker_iterator_next (iterator, &insn);
 
     // for some reason the range exclusion does not work reliably on iOS (?)
@@ -55,7 +57,7 @@ module.exports = new CModule(`
     uintptr_t cur_loc = cpu_context->${pc} - ud->base;
     uintptr_t prev_loc = ud->prev_loc;
     uint8_t * afl_area_ptr = ud->afl_area_ptr;
-    
+
     cur_loc  = (cur_loc >> 4) ^ (cur_loc << 8);
     cur_loc &= 65536 - 1;
     afl_area_ptr[cur_loc ^ prev_loc]++;
